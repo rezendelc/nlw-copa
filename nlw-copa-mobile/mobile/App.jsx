ba79221a0e97087015194e8efe29efaacdcd2903
Lucas Rezende <rezendelc94@gmail.com>
2022-11-03 17:07:20 -0300

    Third Class

--- nlw-copa-mobile/mobile/App.jsx
+++ nlw-copa-mobile/mobile/App.jsx
@@ -2,8 +2,10 @@ import { NativeBaseProvider, StatusBar } from "native-base";
 import { THEME } from './src/styles/theme'
 import { useFonts, Roboto_400Regular, Roboto_500Medium, Roboto_700Bold } from '@expo-google-fonts/roboto'
 
+import { AuthContextProvider } from "./src/context/AuthContext";
+
 import { Loading } from './src/components/Loading';
-import { SignIn } from './src/screens/Signin';
+import { Pools } from './src/screens/Pools';
 
 export default function App() {
 
@@ -11,12 +13,14 @@ export default function App() {
 
   return (
     <NativeBaseProvider theme={THEME}>
-      <StatusBar 
-        barStyle="light-content"
-        backgroundColor="transparent"
-        translucent
-      />
-      { fontsLoaded ? <SignIn/> : <Loading/> }
+      <AuthContextProvider>
+        <StatusBar 
+          barStyle="light-content"
+          backgroundColor="transparent"
+          translucent
+          />
+        { fontsLoaded ? <Pools/> : <Loading/> }
+      </AuthContextProvider>
     </NativeBaseProvider>
   );
 }
